26502b0ab27e3ea6676d239aa6d84df7fe6a4dc9
Ma28hew <98984621+Ma28hew@users.noreply.github.com>
2022-03-18 12:12:38 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -3,29 +3,44 @@
 // Variables to Store HTML Elements
 let Magic8BallEl = document.getElementById("Magic8Ball");
 let resultsEl = document.getElementById("result");
+let inpQ = document.getElementById("input");
 
 // Event Listeners
 Magic8BallEl.addEventListener("click", result);
 
 // Event Functions
 function result() {
+    let inpEl = inpQ.value;
+    inpEl = inpEl.toLowerCase();
 
-    // Process
-    let results = resultsEl.value;
+
+    if (inpEl === "") {
+        resultsEl.innerHTML = "Please ask a question."
+    } else if (inpEl === "do you work" || inpEl === "does a magic 8 ball really work?") {
+        resultsEl = "How dare you doubt me!"
 
 
-    let randNum = Math.random();
-    console.log(randNum);
 
-    if (randNum < 0.2) {
-        results.innerHTML = "Without a Doubt."
-    } else if (randNum < 0.4) {
-        results.innerHTML = "As I see it, yes."
-    } else if (randNum < 0.6) {
-        results.innerHTML = "Concentrate and ask again."
-    } else if (randNum < 0.8) {
-        results.innerHTML = "Don't count on it"
+
+
+
+
     } else {
-        results.innerHTML = "Outlook not so good."
+
+
+        let randNum = Math.random();
+        console.log(randNum);
+
+        if (randNum < 0.2) {
+            resultsEl.innerHTML = "Without a Doubt."
+        } else if (randNum < 0.4) {
+            resultsEl.innerHTML = "As I see it, yes."
+        } else if (randNum < 0.6) {
+            resultsEl.innerHTML = "Concentrate and ask again."
+        } else if (randNum < 0.8) {
+            resultsEl.innerHTML = "Don't count on it"
+        } else {
+            resultsEl.innerHTML = "Outlook not so good."
+        }
     }
 }
